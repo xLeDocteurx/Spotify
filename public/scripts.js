26a1af19e67e236a0700d9f7afe814a6e61abe1c
xLeDocteurx <lenoirc.fr@gmail.com>
2018-07-04 22:07:17 +0200

    initialised npm in the public directory to get frameworks clien t side. finished the server.js file

---
+++ public/scripts.js
@@ -0,0 +1,1 @@
+let yourself = 'go';
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const ejs = require('ejs');
+// const ejs = require('ejs');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyparser.urlencoded({ extended: false }));
+
+let server = app.listen(process.env.PORT || 8080);
+
+app.get('/', (req, res) => {
+
+    res.render('./');
+});
