31cf8f318c1f9980ef23b5200baa325202e74200
xLeDocteurx <lenoirc.fr@gmail.com>
2018-07-07 16:28:03 +0200

    started working on the caroussel. there is a bug in the disconnection process

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,8 @@
 AUTH.do("check_auth", () => {
   let ucolor = JSON.parse(localStorage.getItem("user")).color;
 
-//   document.body.style.color = ucolor;
-let header = document.getElementById("fixed-header");
+  //   document.body.style.color = ucolor;
+  let header = document.getElementById("fixed-header");
   header.style.backgroundColor = ucolor;
   header.style.opacity = 0.6;
-
 });
