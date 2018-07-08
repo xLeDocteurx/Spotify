f39824c63c999ef78ea7a73a6b959bcea7d0a616
xLeDocteurx <lenoirc.fr@gmail.com>
2018-07-08 03:37:19 +0200

    css fixe + some work on the custom layout using user's color into the gradients of the page

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,11 @@ AUTH.do("check_auth", () => {
   let ucolor = JSON.parse(localStorage.getItem("user")).color;
 
   //   document.body.style.color = ucolor;
-  let header = document.getElementById("fixed-header");
+  let header = document.getElementById("nav-brand");
   header.style.backgroundColor = ucolor;
-  header.style.opacity = 0.6;
+  // header.style.opacity = 0.6;
+  let gradients = document.getElementsByClassName("gradient");
+  for (let i =0; i < gradients.length; i++) {
+  gradients[i].style.backgroundImage = `-webkit-linear-gradient(50deg, #ff4169 0, ${ucolor} 100%)`;
+}
 });
